969e59b11dfd0da9ee7004623cc83ddd7ff49a71
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2016-04-14 21:04:05 +0200

    Add hardcoded movement values and basic movement logic

--- app/scripts/controllers/movement.js
+++ app/scripts/controllers/movement.js
@@ -17,5 +17,26 @@ angular.module('hexMapJsApp')
     // Add space ship in the center
     hexagonGrid.addSprite(0, 0, 'images/space_ship_200x200.png');
 
+    // Hardcoded ship characteristics
+    // var current_position = {u: 0, v: 0};
+    var foreseenDestination = {u: -3, v: 0};
+    // var orientation = {u: -1, v: 0};
+    var manoeuvrability = 2;
+
+    // Compute allowed destinations
+    var allowedDestinations = [];
+    for (var allowedU = foreseenDestination.u - manoeuvrability; allowedU <= foreseenDestination.u + manoeuvrability; allowedU++) {
+      for (var allowedV = foreseenDestination.v - manoeuvrability; allowedV <= foreseenDestination.v + manoeuvrability; allowedV++) {
+        var allowedDestination = {u: allowedU, v: allowedV};
+        if (hexagonGrid.Distance(foreseenDestination, allowedDestination) > manoeuvrability){
+          continue;
+        }
+        allowedDestinations.push(allowedDestination);
+      }
+    }
+    for (var i = 0; i < allowedDestinations.length; i++){
+      hexagonGrid.setHexColor(allowedDestinations[i].u, allowedDestinations[i].v, 'green');
+    }
+
     $scope.hexagonGrid = hexagonGrid;
   });
